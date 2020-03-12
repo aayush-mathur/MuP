2789b2d7b1ace728feb52d2b2e184e137d864c10
aayush-mathur <aayushmathur05@gmail.com>
2020-03-12 10:52:23 +0530

    Added tue lab3

--- Labs/Lab3/tuAN3.asm
+++ Labs/Lab3/tuAN3.asm
@@ -1,3 +1,9 @@
+; Write an ALP that will take in user input via the keyboard, character by character if the character entered is in CAPS it should be converted to small alphabets and displayed, else displayed as it is. This program should run until the user presses 'enter'. Enter should not be displayed.
+
+; For e.g.
+
+; If user enters miCROprocessor and INTERFAcing (followed by enter) what will be displayed on screen is microprocessor and interfacing. [Pl note the original string will not be displayed as the user enters it]
+
 .model tiny
 .486
 .code
